ec97f826bf16a965193197f08965924153cd5590
EunbiYoon <yinyinbei0717@gmail.com>
2022-08-14 20:01:15 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template,request,redirect,url_for
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'POST':
+        if request.form.get('action1') == 'quality':
+            return render_template('item/quality.html') # do something
+        elif  request.form.get('action2') == 'vision':
+            return render_template('item/visiion.html') # do something
+        elif  request.form.get('action3') == 'service':
+            return render_template('item/service.html') # do something
+        elif  request.form.get('action4') == 'product':
+            return render_template('item/product.html') # do something
+        elif  request.form.get('action5') == 'audit':
+            return render_template('item/audit.html') # do something
+        elif  request.form.get('action6') == 'email':
+            return render_template('item/email.html') # do something
+        elif  request.form.get('action7') == 'website':
+            return render_template('item/website.html') # do something
+        else:
+            pass # unknown
+    elif request.method == 'GET':
+        return render_template('index.html')
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
